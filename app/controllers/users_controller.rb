16747019add7f2c2bc89de422fac36e0245c3b40
jucchann <kanakosuzuki.315@gmail.com>
2020-08-06 14:28:37 +0900

    サインアップ機能の実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,15 @@
 class UsersController < ApplicationController
+
+    def new
+      @user = User.new
+    end
+
+    def create
+      @user = User.new(sign_up_params)
+      if @user.valid?
+    end
+
+  def index
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
+  root "items#index"
+
+  resources :users, only: :index
+
 end
