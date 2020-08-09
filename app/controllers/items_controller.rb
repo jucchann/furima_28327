1163607f9db6d9bf9f12664fae5aa2a1062b6ace
jucchann <kanakosuzuki.315@gmail.com>
2020-08-09 16:11:22 +0900

    createアクション定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,4 +8,14 @@ class ItemsController < ApplicationController
     @item = Item.new
   end
 
+  def create
+    Item.create(item_params)
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image, :name, :description, :price)
+  end
+
 end
