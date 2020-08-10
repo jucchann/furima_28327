6c010de43f838e7ebf93d38d9ef4506de1efae98
jucchann <kanakosuzuki.315@gmail.com>
2020-08-10 14:51:39 +0900

    バリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,26 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one :purchase
+  has_one_attached :image
+
+  with_options presence: true do
+    validates :image
+    validates :name
+    validates :description
+    validates :price
+  end
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :condition
+  belongs_to_active_hash :delivery_fee
+  belongs_to_active_hash :delivery_from
+  belongs_to_active_hash :delivery_days
+
+  validates :image, :name, :description, :price, :category, :condition, :delivery_fee, :delivery_from, :delivery_days, presence: true
+
+  #選択が「--」のままになっていないか
+  with_options numericality: { other_than: 1 } do
+    validates :category_id, :condition_id, :delivery_fee_id, :delivery_from_id, :delivery_days_id
+  end
 end
