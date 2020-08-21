52d43769370a55d0608b7745d488bbcaca9a7e75
jucchann <kanakosuzuki.315@gmail.com>
2020-08-21 17:01:13 +0900

    show、updateアクションルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   root "items#index"
 
-  resources :users, only: :index
+  resources :users, only: [:index, :show, :update]
   resources :items do
     resources :purchases, only: [:index, :create]
   end
