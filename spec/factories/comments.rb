eec19807b31a575493c3f9b5cf4c9b6bb563c42f
jucchann <kanakosuzuki.315@gmail.com>
2020-08-24 14:57:32 +0900

    commentモデルのテストコード

--- spec/factories/comments.rb
+++ spec/factories/comments.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :comment do
-    
+    text { 'テストです' }
+    association :user
+    association :item
   end
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @comment = FactoryBot.build(:comment)
+  end
+
+  describe 'コメントの保存' do
+    context 'コメントの保存ができるとき' do
+      it '全ての情報があればコメントの保存ができる' do
+      expect(@comment).to be_valid
+      end
+    end
+
+    context 'コメントの保存ができないとき' do
+      it 'textが空だと保存できない' do
+      @comment.text = nil
+      @comment.valid?
+      expect(@comment.errors.full_messages).to include("Textを入力してください")
+      end
+    end
+  end
 end
